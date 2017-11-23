0f0a6867c52271bbca783091c32fcc05db12fb38
Nargonath <jonas.pauthier@gmail.com>
2017-11-23 14:59:25 +0100

    Add template chapter

---
+++ src/app/menu/menu.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'pr-menu',
+  templateUrl: './menu.component.html',
+  styleUrls: ['./menu.component.css'],
+})
+export class MenuComponent implements OnInit {
+  public navbarCollapsed: Boolean = true;
+
+  constructor() {}
+
+  ngOnInit() {}
+
+  toggleNavbar() {
+    this.navbarCollapsed = !this.navbarCollapsed;
+  }
+}
